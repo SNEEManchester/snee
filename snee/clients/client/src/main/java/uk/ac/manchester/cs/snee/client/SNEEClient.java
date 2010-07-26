5af5ae8feecf4d8b39ef2605d94522c9ca099158
alasdair.gray <alasdair.gray@43248f56-cd0b-11dd-b6f5-21ad4398c266>
2010-07-26 13:22:38 +0000

    Ported over the command line clients for testing the SNEE compiler.

---
+++ snee/clients/client/src/main/java/uk/ac/manchester/cs/snee/client/SNEEClient.java
@@ -0,0 +1,118 @@
+package uk.ac.manchester.cs.snee.client;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
+import org.apache.log4j.Logger;
+
+import uk.ac.manchester.cs.snee.SNEE;
+import uk.ac.manchester.cs.snee.SNEEController;
+import uk.ac.manchester.cs.snee.SNEEException;
+import uk.ac.manchester.cs.snee.common.SNEEConfigurationException;
+import uk.ac.manchester.cs.snee.compiler.metadata.schema.SchemaMetadataException;
+import uk.ac.manchester.cs.snee.evaluator.EvaluatorException;
+import uk.ac.manchester.cs.snee.evaluator.StreamResultSet;
+import uk.ac.manchester.cs.snee.evaluator.StreamResultSetImpl;
+import uk.ac.manchester.cs.snee.evaluator.types.Output;
+
+public abstract class SNEEClient implements Observer {
+
+	protected static Logger logger = 
+		Logger.getLogger(SNEEClient.class.getName());
+	protected SNEE controller;
+	protected String _query;
+	protected double _duration;
+	protected long _sleepDuration;
+
+	public SNEEClient(String query, double duration) 
+	throws SNEEException, IOException, SNEEConfigurationException {
+		if (logger.isDebugEnabled())
+			logger.debug("ENTER SNEEClient() with query " + query + 
+					" duration " + duration);
+		
+		_query = query;
+		_duration = duration;
+		controller = new SNEEController("etc/snee.properties");
+
+		if (logger.isDebugEnabled())
+			logger.debug("RETURN SNEEClient()");
+	}
+
+	private static void printResults(Collection<Output> results, int queryId) {
+		System.out.println("\n\n************ Results for query " + queryId + " ************\n\n");
+		for (Output output : results) {
+			System.out.println(output);
+		}
+		System.out.println("\n\n*********************************\n\n");
+	}
+
+	@SuppressWarnings("unchecked")
+	public void update (Observable observation, Object arg) {
+		if (logger.isDebugEnabled())
+			logger.debug("ENTER update() with " + observation + " " + arg);
+		logger.trace("arg type: " + arg.getClass());
+		if (arg instanceof Collection<?>) {
+			Collection<Output> results = (Collection<Output>) arg;
+			printResults(results, 1);
+		} else if (arg instanceof Output) {
+			Output output = (Output) arg;
+			System.out.println(output);
+		}
+		if (logger.isDebugEnabled())
+			logger.debug("RETURN update()");
+	}
+	
+	public void run() 
+	throws SNEEException, SchemaMetadataException, EvaluatorException {
+		if (logger.isDebugEnabled()) 
+			logger.debug("ENTER");
+		System.out.println("Query: " + this._query);
+
+		//		try {
+		int queryId1 = controller.addQuery(_query);
+		//		int queryId2 = controller.addQuery(query);
+
+		long startTime = System.currentTimeMillis();
+		long endTime = (long) (startTime + (_duration * 1000));
+
+		System.out.println("Running query for " + _duration + 
+			" seconds.");
+
+		StreamResultSetImpl resultSet = 
+			(StreamResultSetImpl) controller.getResultSet(queryId1);
+		resultSet.addObserver(this);
+		
+		try {			
+			Thread.currentThread().sleep((long)_duration * 1000);
+		} catch (InterruptedException e) {
+		}
+		
+		while (System.currentTimeMillis() < endTime) {
+			Thread.currentThread().yield();
+		}
+		
+		List<Output> results1 = displayFinalResult(queryId1);
+
+		controller.close();
+		printResults(results1, queryId1);
+		//		printResults(results2, queryId2);
+		if (logger.isDebugEnabled())
+			logger.debug("RETURN");
+	}
+
+	private List<Output> displayFinalResult(int queryId1) throws SNEEException {
+		StreamResultSet resultSet1 = controller.getResultSet(queryId1);
+		List<Output> results1 = resultSet1.getResults();
+		//		Collection<Output> results2 = controller.getResults(queryId2);
+		System.out.println("Stopping query " + queryId1 + ".");
+		controller.removeQuery(queryId1);
+		//		System.out.println("Query run for required duration. " +
+		//				"Stopping query " + queryId2 + ".");
+		//		controller.removeQuery(queryId2);
+		return results1;
+	}
+
+}

26177d8f302cf387e81957f6d3ce122f8b24ee0d
das.bobby2000@gmail.com <das.bobby2000@gmail.com@43248f56-cd0b-11dd-b6f5-21ad4398c266>
2010-11-09 11:14:34 +0000

    Added failure conditions for the node controller. Simplified the error handling code.

--- sncb/src/main/resources/etc/sncb/tools/python/utils/error_handling.py
+++ sncb/src/main/resources/etc/sncb/tools/python/utils/error_handling.py
@@ -1,15 +1,11 @@
 import sys
 
 E_SUCCESS = 0
-E_INSTALLATION_ERROR = 1
-E_MAKE_ERROR = 2
+E_FAIL = 2
 
 def handleReturnCode(retcode, msg=None):
     if retcode == E_SUCCESS:
         print 'E_SUCCESS'        
-    elif retcode == E_INSTALLATION_ERROR:
-        print 'E_INSTALLATION_ERROR: ' + str(msg)
-        sys.exit(E_INSTALLATION_ERROR)        
-    elif retcode == E_MAKE_ERROR:
-        print 'E_MAKE_ERROR: ' + str(msg)
-        sys.exit(E_MAKE_ERROR)
+    else:
+        print 'E_FAIL: ' + str(msg)
+        sys.exit(E_FAIL)

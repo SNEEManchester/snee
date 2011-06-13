c9b406a224e65fc938ea1d7b1b09c1a89819fae7
praveengs@gmail.com <praveengs@gmail.com@43248f56-cd0b-11dd-b6f5-21ad4398c266>
2011-06-13 17:26:15 +0000

    To set and get operatorSourceType

--- snee-core/src/main/java/uk/ac/manchester/cs/snee/operators/logical/DStreamOperator.java
+++ snee-core/src/main/java/uk/ac/manchester/cs/snee/operators/logical/DStreamOperator.java
@@ -79,6 +79,7 @@ implements LogicalOperator {
 		this.setOperatorName("DSTREAM");
 //		this.setNesCTemplateName("istream never set");
 		this.setOperatorDataType(OperatorDataType.STREAM);
+		this.setOperatorSourceType(inputOperator.getOperatorSourceType());
 		this.setParamStr("");
 
 		setChildren(new LogicalOperator[] {inputOperator});

7036a27834cb661d70ad157d9ec8d5777afbcc1b
praveengs@gmail.com <praveengs@gmail.com@43248f56-cd0b-11dd-b6f5-21ad4398c266>
2011-06-13 18:07:56 +0000

    Performed Valve and Exchange operator insertion. Also done the combining of select and join operators.

--- compiler/src/main/java/uk/ac/manchester/cs/snee/compiler/rewriter/LogicalRewriter.java
+++ compiler/src/main/java/uk/ac/manchester/cs/snee/compiler/rewriter/LogicalRewriter.java
@@ -13,23 +13,41 @@ import uk.ac.manchester.cs.snee.compiler.queryplan.LAF;
 import uk.ac.manchester.cs.snee.compiler.queryplan.TraversalOrder;
 import uk.ac.manchester.cs.snee.compiler.queryplan.expressions.Attribute;
 import uk.ac.manchester.cs.snee.compiler.queryplan.expressions.Expression;
+import uk.ac.manchester.cs.snee.compiler.queryplan.expressions.NoPredicate;
+import uk.ac.manchester.cs.snee.metadata.MetadataManager;
+import uk.ac.manchester.cs.snee.metadata.schema.AttributeType;
+import uk.ac.manchester.cs.snee.metadata.schema.SchemaMetadataException;
+import uk.ac.manchester.cs.snee.metadata.schema.TypeMappingException;
+import uk.ac.manchester.cs.snee.metadata.source.SourceType;
+import uk.ac.manchester.cs.snee.operators.logical.ExchangeOperator;
+import uk.ac.manchester.cs.snee.operators.logical.JoinOperator;
 import uk.ac.manchester.cs.snee.operators.logical.LogicalOperator;
 import uk.ac.manchester.cs.snee.operators.logical.RStreamOperator;
+import uk.ac.manchester.cs.snee.operators.logical.SelectOperator;
+import uk.ac.manchester.cs.snee.operators.logical.ValveOperator;
+import uk.ac.manchester.cs.snee.operators.logical.WindowOperator;
 
 public class LogicalRewriter {
 
 	
 	Logger logger = Logger.getLogger(this.getClass().getName());
 	
-	public LogicalRewriter () {
+	private AttributeType _boolType;
+	
+	public LogicalRewriter (MetadataManager metadata) throws TypeMappingException, SchemaMetadataException {
 		if (logger.isDebugEnabled())
 			logger.debug("ENTER LogicalRewriter()");
+		
+		if (logger.isTraceEnabled()) {
+			logger.trace("#extents=" + metadata.getExtentNames().size());
+		}
+		_boolType = metadata.getTypes().getType("boolean");
 		if (logger.isDebugEnabled())
 			logger.debug("RETURN LogicalRewriter()");
 	}
 	
 	public LAF doLogicalRewriting(LAF laf) throws SNEEConfigurationException,
-	OptimizationException {
+	OptimizationException, SchemaMetadataException, AssertionError, TypeMappingException {
 		if (logger.isDebugEnabled())
 			logger.debug("ENTER doLogicalRewriting() laf="+laf.getID());
 		String lafName = laf.getID();
@@ -45,13 +63,130 @@ public class LogicalRewriter {
 	    	removeUnrequiredOperators(laf);
 	    	removeRStream(laf);
 	    }
+	    //Praveen COde Begins
+	    boolean combineSelectandJoin = true;//need to convert to SNEE property if allowed
+	    if (combineSelectandJoin) {
+	    	combineSelectandJoin(laf);
+	    }
+	    insertExchangeOperators(laf);
+	    insertValveOperator(laf);
+	    //Praveen Code Ends
 		if (logger.isDebugEnabled())
 			logger.debug("RETURN doLogicalRewriting()");
 		return laf;
 	}
 	
 	
-    /** 
+	/**
+	 * In case there are predicates defined for a selection which is immediately
+	 * preceded by a join condition, the following method combines both the operations
+	 * into a single join operator with the appropriate predicates set into the join
+	 * condition
+	 * 
+	 * @param laf
+	 * @throws SchemaMetadataException
+	 * @throws AssertionError
+	 * @throws TypeMappingException
+	 * @throws OptimizationException
+	 */
+	private void combineSelectandJoin(LAF laf) throws SchemaMetadataException,
+			AssertionError, TypeMappingException, OptimizationException {
+		Iterator<LogicalOperator> opIter = laf
+				.operatorIterator(TraversalOrder.POST_ORDER);
+		LogicalOperator childOperator;
+		LogicalOperator op;		
+		while (opIter.hasNext()) {
+			op = opIter.next();
+			if (op instanceof SelectOperator) {
+				childOperator = op.getInput(0);
+				if (!(op.getPredicate() instanceof NoPredicate)
+						&& childOperator instanceof JoinOperator) {
+					childOperator.setPredicate(op.getPredicate());
+					logger.trace("Removing node " + op.getText());
+					laf.removeOperator(op);
+				}
+			}		
+
+		}
+
+	}
+
+	/**
+	 * This method accepts the laf tree. It does a post order traversal
+	 * on it. During the traversal, if nodes are found whose children
+	 * are Push operators, then a valve operator is inserted between the parent
+	 * and the child to avoid the overwhelming of the parent operator with the
+	 * incoming data
+	 * 
+	 * @param laf
+	 */
+	private void insertValveOperator(LAF laf) {
+		Iterator<LogicalOperator> opIter = laf
+				.operatorIterator(TraversalOrder.POST_ORDER);
+		LogicalOperator childOperator;
+		LogicalOperator op;
+		Iterator<LogicalOperator> iter;
+		while (opIter.hasNext()) {
+			op = opIter.next();
+			iter = op.childOperatorIterator();
+			if (!(op instanceof WindowOperator) && iter.hasNext()) {
+				do {
+					childOperator = iter.next();
+					SourceType opSourceType = childOperator
+							.getOperatorSourceType();
+					if ((opSourceType == SourceType.PULL_STREAM_SERVICE
+							|| opSourceType == SourceType.UDP_SOURCE 
+							|| opSourceType == SourceType.PUSH_STREAM_SERVICE)) {
+
+						laf.getOperatorTree().insertNode(childOperator, op,
+								new ValveOperator(childOperator, _boolType));
+
+					}
+				} while (iter.hasNext());
+
+			}
+
+		}
+
+	}
+
+	/**
+	 * This method inserts exchange operators between operators, where a boundary
+	 * is found between a sensor network and a non-sensor network.
+	 * 
+	 * @param laf
+	 */
+	private void insertExchangeOperators(LAF laf) {
+
+		Iterator<LogicalOperator> opIter = laf
+				.operatorIterator(TraversalOrder.POST_ORDER);
+		LogicalOperator childOperator;
+		LogicalOperator op;
+		Iterator<LogicalOperator> iter;
+		while (opIter.hasNext()) {
+			op = opIter.next();
+			iter = op.childOperatorIterator();
+			if (!(op instanceof WindowOperator) && iter.hasNext()) {
+				do {
+					childOperator = iter.next();
+					SourceType opSourceType = childOperator
+							.getOperatorSourceType();
+					if (opSourceType == SourceType.SENSOR_NETWORK 
+							&& op.getOperatorSourceType() != opSourceType) {
+
+						laf.getOperatorTree().insertNode(childOperator, op,
+								new ExchangeOperator(childOperator, _boolType));
+
+					}
+				} while (iter.hasNext());
+
+			}
+
+		}
+		
+	}
+
+	/** 
      * Removes unrequired operators.
      * Removes operators that report they are removeable.
      * Removal operators are ones that have the same physical input as output.

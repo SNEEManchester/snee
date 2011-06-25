85b863b5a4d4acdad49ecc208859b8948df684ac
praveengs@gmail.com <praveengs@gmail.com@43248f56-cd0b-11dd-b6f5-21ad4398c266>
2011-06-25 21:56:24 +0000

    New Queue implementation, this behaves as a proper queue, and increases in size when more elements arrive

---
+++ snee-core/src/main/java/uk/ac/manchester/cs/snee/common/SNEEQueue.java
@@ -0,0 +1,197 @@
+package uk.ac.manchester.cs.snee.common;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.AbstractQueue;
+
+import org.apache.log4j.Logger;
+
+public class SNEEQueue<E> extends AbstractQueue<E> {
+	
+	private Logger logger = 
+		Logger.getLogger(this.getClass().getName());
+	
+	private E[] array;
+
+	private int firstIndex;
+	private int lastIndex;
+	private int numElements = 0;
+	private boolean full;
+	private boolean isIncreaseQueueCapacityEnabled;	
+	private int capacityToIncrease;
+
+	public SNEEQueue(int capacity) {
+		firstIndex = 0;
+		lastIndex = 0;
+		// this.capacity = capacity;
+		full = false;
+		array = newElementArray(capacity);
+		//TODO set property for isIncreaseQueueCapacityEnabled
+		isIncreaseQueueCapacityEnabled = true;
+		capacityToIncrease = 10;
+	}
+
+	@SuppressWarnings("unchecked")
+	private E[] newElementArray(int size) {
+		return (E[]) new Object[size];
+	}
+
+	@Override
+	public boolean offer(E object) {
+		if (full) {
+			if (isIncreaseQueueCapacityEnabled) {
+				increaseQueueCapacity(capacityToIncrease);
+			} else {
+				return false;
+			}
+		}
+		array[lastIndex] = object;
+		//System.out.println("Added"+object);
+		numElements++;
+		increaseQueueSize();
+		return true;
+	}
+
+	private void increaseQueueCapacity(int incCapacity) {
+		// TODO Auto-generated method stub
+		E[] newArray = newElementArray(array.length);
+		Iterator<E> qIterator = this.iterator();
+		int i = 0;
+		if (qIterator.hasNext()) {
+			do {
+				//System.out.println("i: "+i);
+				if (logger.isDebugEnabled()) {
+					logger.debug("i: "+i);
+				}
+				newArray[i++] = qIterator.next();
+				//System.out.println("Element: "+newArray[i - 1]);
+				if (logger.isDebugEnabled()) {
+					logger.debug("Element: "+newArray[i - 1]);
+				}
+			} while (qIterator.hasNext());
+		}		
+		//System.out.println("Current Capacity: "+ capacity());
+		if (logger.isDebugEnabled()) {
+			logger.debug("Current Capacity: "+ capacity());
+		}
+		array = Arrays.copyOf(newArray, capacity()+ incCapacity);
+		//System.out.println("Current Capacity: "+ capacity());
+		if (logger.isDebugEnabled()) {
+			logger.debug("Current Capacity: "+ capacity());
+		}
+		firstIndex = 0;
+		lastIndex = i;
+		full = false;
+	}
+
+	private void increaseQueueSize() {
+		lastIndex = increaseSize(lastIndex);
+		// lastIndex = (lastIndex + 1) % capacity();
+		full = (lastIndex == firstIndex);
+	}
+
+	private int increaseSize(int index) {
+		return (index + 1) % capacity();
+	}
+
+	@Override
+	public E poll() {
+		E object = null;
+		if (size() > 0) {
+			object = array[firstIndex];
+			decreaseQueueSize();
+		}
+		return object;
+
+	}
+
+	private void decreaseQueueSize() {
+		// firstIndex = (firstIndex + 1) % capacity();
+		firstIndex = increaseSize(firstIndex);
+		full = false;
+	}
+
+	@Override
+	public E peek() {
+		E object = null;
+		if (size() > 0) {
+			object = array[firstIndex];
+		}
+		return object;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		return new QueueIterator();
+	}
+
+	@Override
+	public int size() {
+		if (full) {
+			return capacity();
+		}
+
+		if (lastIndex >= firstIndex) {
+			return lastIndex - firstIndex;
+		} else {
+			return lastIndex - firstIndex + capacity();
+		}
+	}
+
+	public int capacity() {
+		return array.length;
+	}
+	
+	public void printIndexes() {
+		System.out.println("Last Index: "+ lastIndex);
+		System.out.println("First Index: "+ firstIndex);
+	}
+
+	private class QueueIterator implements Iterator<E> {
+
+		private int nextPosition;
+		private int last;		
+		private boolean isFirst = true;
+		private int numberElements;
+
+		public QueueIterator() {
+			nextPosition = firstIndex;
+			numberElements = size();
+		}
+
+		@Override
+		public boolean hasNext() {
+			boolean hasNext = false;
+			if (numberElements > 0) {
+				if (isFirst) {
+					//nextPosition = firstIndex;
+					last = nextPosition; 
+					nextPosition = increaseSize(nextPosition);
+					hasNext = true;
+					isFirst = false;
+				} else if (nextPosition == lastIndex) {
+					hasNext = false;
+				} else {
+					last = nextPosition; 
+					nextPosition = increaseSize(nextPosition);
+					hasNext = true;					
+				}
+
+			}
+			return hasNext;
+		}
+		
+		@Override
+		public E next() {
+			// TODO Auto-generated method stub
+			return array[last];
+		}
+
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+
+	}
+
+}

e2ab65f35d4f539f1f8dbcccca3e59a079e11565
alasdair.gray <alasdair.gray@43248f56-cd0b-11dd-b6f5-21ad4398c266>
2011-06-30 15:04:05 +0000

    Provided documentation for the methods

--- snee-api/src/main/java/uk/ac/manchester/cs/snee/datasource/webservice/PullSourceWrapper.java
+++ snee-api/src/main/java/uk/ac/manchester/cs/snee/datasource/webservice/PullSourceWrapper.java
@@ -11,11 +11,36 @@ import uk.ac.manchester.cs.snee.metadata.schema.TypeMappingException;
 
 public interface PullSourceWrapper extends SourceWrapper {
 
+	/**
+	 * Retrieve data for a given {@code PullSource} resource from a specified 
+	 * point of time ranging forward a specified number of tuples.
+	 * 
+	 * @param resourceName name of the resource to retrieve values from
+	 * @param numItems number of items to retrieve
+	 * @param timestamp point in time from which to gather tuples
+	 * @return A tuple list with the returned data values
+	 * @throws SNEEDataSourceException
+	 * @throws TypeMappingException
+	 * @throws SchemaMetadataException
+	 * @throws SNEEException
+	 */
 	public List<Tuple> getData(String resourceName, int numItems, 
 		Timestamp timestamp) 
 	throws SNEEDataSourceException, TypeMappingException,
 		SchemaMetadataException, SNEEException;
 	
+	/**
+	 * Retrieve the specified number of newest data items for a specified 
+	 * {@code PullSource} resource
+	 * 
+	 * @param resourceName name of the resource to retrieve values from
+	 * @param numItems number of items to retrieve
+	 * @return
+	 * @throws SNEEDataSourceException
+	 * @throws TypeMappingException
+	 * @throws SchemaMetadataException
+	 * @throws SNEEException
+	 */
 	public List<Tuple> getNewestData(String resourceName, 
 		Integer numItems) 
 	throws SNEEDataSourceException, TypeMappingException, 

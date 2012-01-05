598a45a95346d26aa75ed43a63147dc8b48c591d
alan.barry.stokes@gmail.com <alan.barry.stokes@gmail.com@43248f56-cd0b-11dd-b6f5-21ad4398c266>
2012-01-05 18:28:43 +0000

    added functionality to both avrora versions (mica2 and micaz) QU?? why is it they share code and we have not merged them into one class? or at least given them a shared class for the common code?

--- sncb/src/main/java/uk/ac/manchester/cs/snee/sncb/TinyOS_SNCB_Avrora_Mica2.java
+++ sncb/src/main/java/uk/ac/manchester/cs/snee/sncb/TinyOS_SNCB_Avrora_Mica2.java
@@ -1,6 +1,7 @@
 package uk.ac.manchester.cs.snee.sncb;
 
 import java.io.BufferedInputStream;
+import java.io.IOException;
 
 import uk.ac.manchester.cs.snee.common.SNEEConfigurationException;
 import uk.ac.manchester.cs.snee.common.SNEEProperties;
@@ -42,53 +43,88 @@ private Process avrora = null;
 	}
  
 	public SerialPortMessageReceiver register(SensorNetworkQueryPlan qep,
-      String queryOutputDir, MetadataManager costParams)
-	throws SNCBException {
-	 	super.register(qep, queryOutputDir, costParams);
-	 	try {
-		 	String avroraCommand = TinyOS_SNCB_Utils.printAvroraCommands(queryOutputDir, qep, 
-		              this.getTargetDirName(), this.target);
-		 	System.out.println(avroraCommand);
-		 	
-	     //   String nescOutputDir = System.getProperty("user.dir") + "/"
-	       // + queryOutputDir + getTargetDirName();
-	        
-	        Runtime rt = Runtime.getRuntime();
-	        avrora = rt.exec("java avrora.Main " + avroraCommand);
-	        BufferedInputStream avroraReader = new BufferedInputStream(avrora.getInputStream());
-	        String outputString = "";
-	        System.out.println("waiting for avrora to initialise");
-	        boolean found = false;
-	        while(!found)
-	        {
-	          byte [] output;
-	          output = new byte[avroraReader.available()];
-	          avroraReader.read(output);
-	          String currentOutputString = new String(output);
-	          System.out.println(currentOutputString);
-	          outputString = outputString.concat(currentOutputString);
-	          String test = "Waiting for serial connection on port 2390...";
-	          if(outputString.contains(test))
-	            found = true;
-			else {
+		      String queryOutputDir, MetadataManager costParams)
+			throws SNCBException 
+			{
+			  super.register(qep, queryOutputDir, costParams);
+			  try 
+			  {
+				String avroraCommand = TinyOS_SNCB_Utils.printAvroraCommands(queryOutputDir, qep, 
+				                       this.getTargetDirName(), this.target);
+				System.out.println(avroraCommand);
+				 	
+			    // String nescOutputDir = System.getProperty("user.dir") + "/"
+			    // + queryOutputDir + getTargetDirName();
+			        
+			    Runtime rt = Runtime.getRuntime();
+			    avrora = rt.exec("java avrora.Main " + avroraCommand);
+			    System.out.println("waiting for avrora to initialise");
+			    BufferedInputStream avroraReader = new BufferedInputStream(avrora.getInputStream());
+			    boolean success = waitForAvrora(avroraReader);
+			    if(success)
+			    {
+			      mr = setUpResultCollector(qep, queryOutputDir);
+			      if (logger.isDebugEnabled())
+			          logger.debug("RETURN register()");
+			        return mr;
+			    }
+			    else
+			    {
+			      if(avrora != null)
+			        avrora.destroy();
+			      throw new SNCBException("avrora failed to get ready");
+			    }
+		      } 
+			  catch (Exception e) 
+			  {
+		        if(avrora != null)
+		          avrora.destroy();
+		        e.printStackTrace();
+		        logger.warn(e.getLocalizedMessage(), e);
+		        throw new SNCBException(e.getLocalizedMessage(), e);
+		      }
+			}
+		 
+		  private boolean waitForAvrora(BufferedInputStream avroraReader) throws IOException, InterruptedException 
+		  { 
+		    String outputString = "";
+			boolean found = false;
+		    while(!found)
+		    {
+		      byte [] output;
+		      output = new byte[avroraReader.available()];
+		      avroraReader.read(output);
+		      String currentOutputString = new String(output);
+		      System.out.println(currentOutputString);
+		      outputString = outputString.concat(currentOutputString);
+		      String test = "Waiting for serial connection on port 2390...";
+		      if(outputString.contains(test))
+		        found = true;
+			  else 
+			  {
+			    if(currentOutputString.equals(""))
+				{
+				  try
+				  {
+				    avrora.exitValue();
+					return false;
+				  }
+				  catch(IllegalThreadStateException e)
+				  {
+					Thread.currentThread();
+					Thread.sleep(5000);
+					return waitForAvrora(avroraReader);
+				  }
+				}
 				Thread.currentThread();
 				Thread.sleep(5000);
-			}
-	        }
-	        System.out.println("avrora ready");
-	        avroraReader.close();
-	        mr = setUpResultCollector(qep, queryOutputDir);
-    } catch (Exception e) {
-        if(avrora != null)
-          avrora.destroy();
-        e.printStackTrace();
-        logger.warn(e.getLocalizedMessage(), e);
-        throw new SNCBException(e.getLocalizedMessage(), e);
-      }
-      if (logger.isDebugEnabled())
-        logger.debug("RETURN register()");
-      return mr;
-	}
+			  }
+		    }
+		    System.out.println("avrora ready");
+		    avroraReader.close();
+		    return true;
+		  }
+
  
 	 
  public void start() throws SNCBException

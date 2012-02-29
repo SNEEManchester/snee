c841b9df5edb4b05d3a127d0e924650b91fda46d
ixentg <ixentg@43248f56-cd0b-11dd-b6f5-21ad4398c266>
2012-02-29 11:24:11 +0000

    configured in-network client for tests with avrora/micaz/elf/PM

--- sncb/src/main/java/uk/ac/manchester/cs/snee/sncb/TinyOS_SNCB_Avrora_Micaz.java
+++ sncb/src/main/java/uk/ac/manchester/cs/snee/sncb/TinyOS_SNCB_Avrora_Micaz.java
@@ -48,7 +48,7 @@ public class TinyOS_SNCB_Avrora_Micaz extends TinyOS_SNCB implements SNCB
 		 	String avroraCommand = TinyOS_SNCB_Utils.printAvroraCommands(queryOutputDir, qep, 
 		              this.getTargetDirName(), this.target, this.avroraPrintDebug);
 		 	System.out.println(avroraCommand);
-		 	//System.exit(0);
+		 	System.exit(0);
 		 	
 	        String nescOutputDir = System.getProperty("user.dir") + "/"
 	        + queryOutputDir + getTargetDirName();
